e44f8da34c7bdae6aaad1901ce6e1ad36ef3a0a1
Igor Gabriel <igorgsbarbosadev@gmail.com>
2025-07-20 14:44:16 -0300

    Consumo de API

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,13 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do filme que deseja buscar: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t="+busca+"&apikey=846994ef";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=846994ef"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
