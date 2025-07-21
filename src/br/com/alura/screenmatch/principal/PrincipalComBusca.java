87c6911aa2018812156337fe76bd93cfd7fab2c1
Igor Gabriel <igorgsbarbosadev@gmail.com>
2025-07-21 19:43:01 -0300

    Screenmatch funcional, realocacao builder Gson

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -13,49 +13,69 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o nome do filme que deseja buscar: ");
-        var busca = scanner.nextLine();
-        try {
-            String endereco = "https://www.omdbapi.com/?t="+busca.replace(" ","+")+"&apikey=846994ef";
+        String busca = "";
 
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        List<Titulo> titulos = new ArrayList<>();
 
-            String json = response.body();
-            System.out.println(json);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
 
-            // Deserializando o JSON para um objeto TituloOmdb
-            // Esse builder faz com que o dado que chega como "Title" seja possivel interpretado
-            // como "title", corrigindo o erro de "null"
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
+        while(!busca.equalsIgnoreCase("sair")) {
 
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(meuTituloOmdb);
-            //try {
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println("Titulo ja convertido:");
-            System.out.println(meuTitulo);
+            System.out.println("Digite o nome do filme que deseja buscar: ");
+            busca = scanner.nextLine();
 
+            if (busca.equalsIgnoreCase("sair")){
+                break;
+            }
 
-        } catch (NumberFormatException e) {
-            System.out.println("Aconteceu um erro:");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e){
-            System.out.println("Algum erro de endereco na busca, verifique o endereco");
-        }catch (ErroDeConversaoDeAnoException e){
-            System.out.println(e.getMensagem());
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=846994ef";
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                System.out.println(endereco);
+                String json = response.body();
+                System.out.println(json);
+
+                // Deserializando o JSON para um objeto TituloOmdb
+                // Esse builder faz com que o dado que chega como "Title" seja possivel interpretado
+                // como "title", corrigindo o erro de "null"
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
+                //try {
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("Titulo ja convertido:");
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro:");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de endereco na busca, verifique o endereco");
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMensagem());
+            }
+
         }
+        System.out.println(titulos);
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
 
         System.out.println("Programa finalizou corretamente.");
 
